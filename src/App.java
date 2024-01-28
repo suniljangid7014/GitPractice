a9eab861af7cd90f3be7323bc75918ef2f92669b
Sunil Kumar <jangidsunil7014@gmail.com>
2024-01-28 22:34:26 +0530

    Added method

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
+	
+	public static void display() {
+		System.out.println("Display Method..");
+	}
+	
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        display();
     }
 }
